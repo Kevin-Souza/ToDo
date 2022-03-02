5fbf64f01d529f7417ac12477e2bf1d5cb761bb1
Kevin-Souza <kevinsouza1.ks@gmail.com>
2022-03-02 19:58:56 -0300

    Adicionar arquivos de projeto.

---
+++ ToDo/Data/AppCont.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Models;
+
+namespace ToDo.Data
+{
+    public class AppCont : DbContext
+    {
+        public AppCont(DbContextOptions<AppCont> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ ToDo/Models/Tarefa.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Models
+{
+    public class Tarefa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        public DateTime EndDate { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
