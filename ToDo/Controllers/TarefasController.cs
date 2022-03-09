387cf64889fb8d953d4002cf67e266890d294dbd
Kevin-Souza <kevinsouza1.ks@gmail.com>
2022-03-09 20:10:59 -0300

    add view Index

---
+++ ToDo/Controllers/TarefasController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDo.Data;
+
+namespace ToDo.Controllers
+{
+    public class TarefasController : Controller
+    {
+        private readonly AppCont _appCont;
+
+        public TarefasController(AppCont appCont)
+        {
+            _appCont = appCont;
+        }
+
+        //Action sincrona, manda uma RC para o servidor e irá esperar a resposta para continuar.
+        public IActionResult Index() // um Método IActionResult espera o retorno de uma view.
+        {
+            var allTasks = _appCont.Tarefas.ToList();
+            // _appCont: caminho do banco,  ToList: seria o select *
+            return View(allTasks);
+        }
+    }
+}
